197af24ad1a63553028f2491ec9abaf6b810ec03
wilgur513 <wilgur513@naver.com>
2022-07-17 01:13:49 +0900

    refactor: 스터디 검색 sql 생성 로직 수정

--- backend/src/main/java/com/woowacourse/moamoa/study/query/StudiesSearcher.java
+++ backend/src/main/java/com/woowacourse/moamoa/study/query/StudiesSearcher.java
@@ -6,6 +6,8 @@ import com.woowacourse.moamoa.study.query.response.StudyResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.RowMapper;
@@ -44,49 +46,35 @@ public class StudiesSearcher {
     }
 
     private String joinTableClause(final SearchingFilters searchingFilters) {
-        String sql = "";
-        if (searchingFilters.hasBy(CategoryName.GENERATION)) {
-            sql += "JOIN study_filter sf1 ON s.id = sf1.study_id "
-                    + "JOIN filter gen_filter ON sf1.filter_id = gen_filter.id "
-                    + "JOIN category c1 ON gen_filter.category_id = c1.id AND c1.name = '" + CategoryName.GENERATION
-                    + "' ";
-        }
-        if (searchingFilters.hasBy(CategoryName.AREA)) {
-            sql += "JOIN study_filter sf2 ON s.id = sf2.study_id "
-                    + "JOIN filter area_filter ON sf2.filter_id = area_filter.id "
-                    + "JOIN category c2 ON area_filter.category_id = c2.id AND c2.name = '" + CategoryName.AREA + "' ";
-        }
-        if (searchingFilters.hasBy(CategoryName.TAG)) {
-            sql += "JOIN study_filter sf3 ON s.id = sf3.study_id "
-                    + "JOIN filter tag_filter ON sf3.filter_id = tag_filter.id "
-                    + "JOIN category c3 ON tag_filter.category_id = c3.id AND c3.name = '" + CategoryName.TAG + "' ";
-        }
-        return sql;
+        String sql = "JOIN study_filter {}_sf ON s.id = {}_sf.study_id "
+                + "JOIN filter {}_f ON {}_sf.filter_id = {}_f.id "
+                + "JOIN category {}_c ON {}_f.category_id = {}_c.id AND {}_c.name = '{}'";
+
+        return Stream.of(CategoryName.values())
+                .filter(searchingFilters::hasBy)
+                .map(name -> sql.replaceAll("\\{\\}", name.name()))
+                .collect(Collectors.joining());
     }
 
     private String filtersInQueryClause(final SearchingFilters searchingFilters) {
-        String sql = "";
-        if (searchingFilters.hasBy(CategoryName.GENERATION)) {
-            sql += "AND gen_filter.id IN (:generationIds) ";
-        }
-        if (searchingFilters.hasBy(CategoryName.AREA)) {
-            sql += "AND area_filter.id IN (:areaIds) ";
-        }
-        if (searchingFilters.hasBy(CategoryName.TAG)) {
-            sql += "AND tag_filter.id IN (:tagIds) ";
-        }
-        return sql;
+        String sql = "AND {}_f.id IN (:{}) ";
+
+        return Stream.of(CategoryName.values())
+                .filter(searchingFilters::hasBy)
+                .map(name -> sql.replaceAll("\\{\\}", name.name()))
+                .collect(Collectors.joining());
     }
 
     private Map<String, Object> params(final String title, final SearchingFilters searchingFilters,
                                        final Pageable pageable) {
+        final Map<String, Object> filterIds = Stream.of(CategoryName.values())
+                .collect(Collectors.toMap(Enum::name, searchingFilters::getFilterIdsBy));
+
         Map<String, Object> param = new HashMap<>();
         param.put("title", "%" + title + "%");
         param.put("limit", pageable.getPageSize() + 1);
         param.put("offset", pageable.getOffset());
-        param.put("generationIds", searchingFilters.getFilterIdsBy(CategoryName.GENERATION));
-        param.put("areaIds", searchingFilters.getFilterIdsBy(CategoryName.AREA));
-        param.put("tagIds", searchingFilters.getFilterIdsBy(CategoryName.TAG));
+        param.putAll(filterIds);
         return param;
     }
 
